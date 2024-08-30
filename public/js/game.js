4b12304c85662815820bf55816cfd50039b164ff
josfam <jamukun@proton.me>
2024-08-30 14:40:53 +0300

    Fix bug with opponent socket, simplify var names
    - Bug where the opponent's socket was undefined is now fixed.
    - Variable names have been greatly simplified on account of there being
      so many of them to keep track of
    - Events emitted by the server when a game is about to begin are fewer

--- public/js/game.js
+++ public/js/game.js
@@ -1,24 +1,36 @@
 window.onload = function() {
   const doc = document;
+  const socket = io();
   //extract page params
   const urlParams = new URLSearchParams(window.location.search);
-  const player1 = urlParams.get('player1');
-  const player2 = urlParams.get('player2');
-  const thisPlayer = urlParams.get('thisPlayer');
-  const room = urlParams.get('roomName');
-  
-  const socket = io();
-  
+  const p1 = urlParams.get('p1');
+  const p2 = urlParams.get('p2');
+  const thisP = urlParams.get('thisP');
+  const otherP = (thisP === p1) ? p2 : p1;
+  const room = '';
+  const thisPMark = (thisP === p1) ? 'X': 'O';
+  const otherPMark = (thisPMark === 'X') ? 'O' : 'X';
+
+  console.log(p1, p2, thisP, otherP, thisPMark, otherPMark);
+
+  const pairingInfo = {
+    thisP,
+    otherP,
+    'challenger': p1,
+    'opponent': p2
+  };
+
+  // const pairing = (thisP === p1) ? {thisP, otherP} : {otherP, thisP};
   // decide opponent role and place name at the bottom of the board
-  socket.emit('game-joined', thisPlayer);
+  socket.emit('game-joined', pairingInfo);
   socket.on('good-luck', (username) => {
     alert(`good luck ${username}`);
   });
 
   // place player names on the board
-  const thisPlayerIndicator = doc.getElementById('this-player-name');
-  thisPlayerIndicator.textContent = thisPlayer;
-  const otherPlayer = (thisPlayer === player1) ? player2 : player1;
-  const otherPlayerIndicator = doc.getElementById('other-player-name')
-  otherPlayerIndicator.textContent = otherPlayer; 
+  const thisPNameDiv = doc.getElementById('this-player-name');
+  thisPNameDiv.textContent = `${thisP} (${thisPMark})`;
+  
+  const otherPIndicator = doc.getElementById('other-player-name')
+  otherPIndicator.textContent = `${otherP} (${otherPMark})`;
 };
