20a2132719539eaf1f0813f28bc1ebfdc5ec57d5
josfam <jamukun@proton.me>
2024-09-07 20:05:35 +0300

    Refactor home.js, Style the user greeting
    - `home.js` uses `common.js` logic
    - The user greeting at home now displays the player's name
    - The greeting and game-play question have been styled

--- public/js/home.js
+++ public/js/home.js
@@ -1,43 +1,24 @@
+import {
+  getUsername,
+  listenForLogout
+} from './common.js'
+
 window.onload = async function() {
   const doc = document;
+  listenForLogout();
   const computerBtn = doc.getElementById('computerbtn');
   const humanBtn = doc.getElementById('humanbtn');
   // logout and home buttons
   const logoutBtn = doc.getElementById('logout-btn');
   let playerName = '';
 
-  logoutBtn.addEventListener('click', async (event) => {
-    const response = await fetch('/api/v1/auth/logout', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    if (response.ok) {
-      alert('Logged out successfully');
-      window.location.href = '/';
-    } else {
-      alert(response.message);
-    }
-  });
+  playerName = await getUsername();
 
-  const getUserName = async function () {
-    const response = await fetch('/api/v1/users/username', {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    const info = await response.json();
-    if (response.ok) {
-      console.log('response is:', response);
-    } else {
-      alert(response.json().message);
-      return;
-    }
-    return info.username;
-  }
-
-  playerName = await getUserName();
+  // Place username in greeting
+  const playerGreetingDiv = doc.getElementById('player-greeting')
+  playerGreetingDiv.textContent = `Hello ${playerName}.`;
+  const questionDiv = doc.getElementById('game-style-question');
+  questionDiv.textContent = `How do you want to play today?`;
 
   computerBtn.addEventListener('click', async (event) => {
     window.location.href = `/computerGame?p1=${playerName}&p1Mark=X&p2=computron&p2Mark=O`;
